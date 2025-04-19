24b3f648b151bd14bb843a32fd79db076ba17e43
DBusuru <davidbusuru1@gmail.com>
2025-04-19 23:28:21 +0300

    checkpoint

---
+++ main.js
@@ -0,0 +1,14 @@
+"use strict";
+class Car {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    start() {
+        console.log("Car engine started");
+    }
+}
+const myCar = new Car("Toyota", "Corolla", 2022);
+myCar.start(); // Should log: "Car engine started"
+// Output: Car engine started
